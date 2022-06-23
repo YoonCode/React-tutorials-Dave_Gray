41c8708fbd530f652eac22b150e87b5d496c1d3d
YoonCode <yjin830201@gmail.com>
2022-06-23 17:55:12 +0900

    learn how to use list and key

--- tutorial/src/Content.js
+++ tutorial/src/Content.js
@@ -1,35 +1,48 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
-  const [name, setName] = useState('Eric')
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: 'One half pound bag of Cocoa Covered Almonds Unsalted',
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2',
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3',
+    },
+  ])
 
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave', 'Eric']
-    const int = Math.floor(Math.random() * 4)
-    setName(names[int])
-  }
-
-  const handleClick = () => {
-    setCount(count + 1)
-    console.log(count)
-  }
-
-  const handleClick2 = () => {
-    console.log(count)
-  }
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText)
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    )
+    setItems(listItems)
   }
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}!</p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click here</button>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
